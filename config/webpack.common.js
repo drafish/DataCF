222443baa42d7925c7b768adda1181cb47dcbeff
xwlyy <xwlyy1991@163.com>
2018-12-29 14:17:03 +0800

    初始化项目，添加webpack配置

---
+++ config/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		'index': path.resolve(__dirname, '../src/index.js'),
+		'deviceId': path.resolve(__dirname, '../src/deviceId.js')
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, '../src/index.html'),
+			inject: false
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'deviceId.html',
+			template: path.resolve(__dirname, '../src/deviceId.html'),
+			inject: false
+		})
+	],
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+		  {
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+		  }
+		]
+	}
+}
