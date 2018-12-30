21e57598db7d692295870337890b72807364a56d
xwlyy <xwlyy1991@163.com>
2018-12-30 22:11:34 +0800

    standard fix config目录

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	entry: {
-		'index': path.resolve(__dirname, '../src/index.js'),
-		'deviceId': path.resolve(__dirname, '../src/deviceId.js')
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: path.resolve(__dirname, '../src/index.html'),
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'deviceId.html',
-			template: path.resolve(__dirname, '../src/deviceId.html'),
-			inject: false
-		})
-	],
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, '../dist'),
-		publicPath: '/'
-	},
-	module: {
-		rules: [
-			{
-				test: /.js$/,
-				enforce: 'post', // post-loader处理
-				loader: 'es3ify-loader'
-			},
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			}
-		]
-	}
+  entry: {
+    'index': path.resolve(__dirname, '../src/index.js'),
+    'deviceId': path.resolve(__dirname, '../src/deviceId.js')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../src/index.html'),
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'deviceId.html',
+      template: path.resolve(__dirname, '../src/deviceId.html'),
+      inject: false
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        enforce: 'post', // post-loader处理
+        loader: 'es3ify-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
