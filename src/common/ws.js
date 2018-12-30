73bd31af6cd85079607c5c40d01de3c44fc0b8cf
xwlyy <xwlyy1991@163.com>
2018-12-30 17:35:55 +0800

    添加common模块:ajax,event,ws,util

---
+++ src/common/ws.js
@@ -0,0 +1,49 @@
+const ws = {};
+
+ws.create = function(url, data) {
+    const socket = new WebSocket(url);
+
+    socket.onopen = function (event) {
+        console.log("Connection open ...");
+        if (data) {
+            socket.send(data)
+        }
+    };
+
+    socket.onmessage = function (event) {
+        console.log("Received Message: " + event.data);
+    };
+
+    socket.onclose = function (event) {
+        console.log("Connection closed.");
+    };
+
+    socket.onerror = function (event) {
+        console.log("Connection error.");
+    };
+
+    ws.socket = socket;
+    ws.url = url;
+}
+
+ws.send = function(data) {
+    if (ws.socket && ws.socket.readyState === WebSocket.OPEN) {
+        ws.socket.send(data);
+    } else {
+        ws.create(ws.url, data);
+    }
+}
+
+ws.close = function() {
+    ws.socket && ws.socket.close && ws.socket.close();
+}
+
+ws.isSupported = function() {
+    if (window.WebSocket !== undefined) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = ws;
