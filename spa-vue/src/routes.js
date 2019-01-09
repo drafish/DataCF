0b78194a2e1a4177a193cb9fb4d53f2136361b65
xwlyy <xwlyy1991@163.com>
2019-01-09 22:43:56 +0800

    mv server,spa-vue,spa-react to DataCS

--- spa-vue/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Foo from './components/Foo'
-import Bar from './components/Bar'
-
-const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
-
-export default routes
